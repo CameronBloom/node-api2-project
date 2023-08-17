6327bfa6aa3532453cb00d5427ffbc02d168f9d6
Cameron <cameron.morgan@skiff.com>
2023-08-17 06:56:28 -0400

    Implemented '[GET] /api/posts'.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,23 @@
 // implement your posts router here
+const express = require("express")
+const router = express.Router()
+const Post = require("./posts-model")
+
+router.get("/", (req, res) => {
+  Post.find()
+    .then(posts => {
+      res.status(200).json(posts)
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "The posts information could not be retrieved",
+        err: err.message,
+        stack: err.stack,
+      })
+    }
+
+    )
+
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -2,7 +2,11 @@
 // require your posts router and connect it here
 const express = require("express")
 const server = express()
+
+const postsRouter = require("./posts/posts-router")
+
 server.use(express.json())
+server.use("/api/posts", postsRouter)
 server.use("*", (req, res) => {
   res.status(404).json({
     message: "endpoint not found"
