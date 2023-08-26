b7fa914fba5b94bb1fe0ad24169cd6d358ff94ae
Cameron <cameron.morgan@skiff.com>
2023-08-25 20:12:44 -0400

    Implemented GET functionality for posts by ID.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -14,10 +14,26 @@ router.get("/", (req, res) => {
         err: err.message,
         stack: err.stack,
       })
+    })
+})
+
+router.get("/:id", async (req, res) => {
+  try {
+    const exists = await Post.findById(req.params.id)
+    if (exists) {
+      res.json(exists)
+    } else {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist",
+      })
     }
-
-    )
-
+  } catch(err) {
+    res.status(500).json({
+      message: "The post information could not be retrieved",
+      err: err.message,
+      stack: err.stack,
+    })
+  }
 })
 
 module.exports = router
